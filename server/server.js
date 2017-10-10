67b72041024b7296334a0aacb826e13b30c4d263
rockjimstar <32493353+rockjimstar@users.noreply.github.com>
2017-10-10 14:19:09 -0600

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,29 +1,3 @@
-/*
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-
-var lionRouter = require('./lions');
-var tigerRouter = require('./tigers');
-
-app.use(morgan('dev'));
-app.use(express.static('client'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use('/lions', lionRouter);
-app.use('/tigers', tigerRouter);
-
-
-app.use(function(err, res, req, next){
-	if(err){
-		console.log(err.message);
-		res.status(500).send(err);
-	}
-});
-*/
 var express = require('express');
 var app = express();
 var api = require('./api/api');
